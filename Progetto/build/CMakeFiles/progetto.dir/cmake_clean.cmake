47d41e149f9a968a9fef994ddf4d5608b18a6e7f
GeckyMerlo <giacomo.merlo@mail.polimi.it>
2025-11-24 15:08:53 +0000

    first commit

---
+++ Progetto/build/CMakeFiles/progetto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progetto.dir/src/plainProgram.cpp.o"
+  "CMakeFiles/progetto.dir/src/plainProgram.cpp.o.d"
+  "progetto"
+  "progetto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/progetto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
